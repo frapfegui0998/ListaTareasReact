a2309ff4ff790e63645345030902f9db5ad82a73
Franz Pfeiffer <frapfegui@gmail.com>
2025-05-12 23:10:22 -0600

    creacion componente perfil
    creacion componente perfil

--- ListaTareas/src/App.jsx
+++ ListaTareas/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tareas from './Tareas';
+import Perfil from './Perfil';
 
 function App() {
-  <BrowserRouter>
-  </BrowserRouter>
+  
   return (
-   <Tareas/>
+    <>
+      <Tareas/>
+      <Perfil/>
+    </>
   )
 }
 
---
+++ ListaTareas/src/Perfil.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Perfil = () => {
+  return (
+    <div>
+      <h2>Acerca del Autor</h2>
+      <p><strong>Nombre:</strong> Franz Pfeiffer Guillén</p>
+      <p><strong>Correo:</strong> frapfegui@gmail.com</p>
+      <p><strong>Descripción:</strong> Autor de este proyecto web</p>
+    </div>
+  );
+};
+
+export default Perfil;
